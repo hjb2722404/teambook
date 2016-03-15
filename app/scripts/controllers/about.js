aac7f53b85f849d660a8ffa8b5d606457b4a238a
xiaolinzi <weilaihui@126.com>
2016-03-15 15:01:14 +0800

    Revert "使用gulp 来构建系统"
    This reverts commit 60173affcf53de3b0419dcc99414edb3f5dabdc7.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myNgAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myNgAppApp
+ */
+angular.module('myNgAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myNgAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myNgAppApp
+ */
+angular.module('myNgAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
